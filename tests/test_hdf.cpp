bac0607320e2d2017e4777eb84a0622fc8b97b1a
johltn <j.luttun@laposte.net>
2021-04-26 09:20:55 +0200

    Store bit values into HDF file

--- tests/test_hdf.cpp
+++ tests/test_hdf.cpp
@@ -6,53 +6,30 @@
 #include <BRepBuilderAPI_MakePolygon.hxx>
 #include <BRepBuilderAPI_MakeFace.hxx>
 #include <BRepPrimAPI_MakeBox.hxx>
+#include <BRep_Builder.hxx>
+#include <BRepBndLib.hxx>
+#include <Bnd_Box.hxx>
+#include <BRepMesh_IncrementalMesh.hxx>
+
+
 
 TEST(HdfFileName, HDF) {
 
-	auto storage = new chunked_voxel_storage<bit_t>(0., 0., 0., 0.1, 1000, 1000, 100, 32);
+	auto storage2 = new chunked_voxel_storage<bit_t>(0., 0., 0., 1, 32, 32, 32, 32);
 
-	for (int i = 0; i < 10; ++i) {
-		for (int j = 0; j < 10; ++j) {
-			BRepPrimAPI_MakeBox mb(gp_Pnt(10 * i, 10 * j, 0.), 8., 8., 8.);
+	BRepPrimAPI_MakeBox mb(gp_Pnt(1, 1, 1), gp_Pnt(10, 10, 10));
+	auto x = mb.Solid();
+	BRepMesh_IncrementalMesh(x, 0.001);
+	auto vox = voxelizer(x, storage2);
+	vox.Convert();
 
-			auto vox = voxelizer(mb.Solid(), storage);
-			vox.Convert();
-		}
-	}
+	hdf_writer writer; 
+	writer.SetVoxels(storage2);
+	writer.Write("voxels.h5");
 
-	/*HDF WRITER*/
-	hdf_writer writer;
-	writer.SetVoxels(storage);
-	writer.Write("test_hdf_writer.vox");
-	/*HDF WRITER*/
+	std::ofstream fs("voxobj.obj");
+	obj_export_helper oeh{ fs };
+	storage2->obj_export(oeh, false, false);
 
-
-	const H5std_string  FILE_NAME("SDS.h5");
-	const H5std_string  DATASET_NAME("IntArray");
-	const int   NX = 5;
-	const int   NY = 6;
-	const int   RANK = 2;
-
-	int i, j;
-	int data[NX][NY];
-	for (j = 0; j < NX; j++)
-	{
-		for (i = 0; i < NY; i++)
-			data[j][i] = i + j;
-	}
-
-	H5::H5File file(FILE_NAME, H5F_ACC_TRUNC);
-	hsize_t     dimsf[2];
-	dimsf[0] = NX;
-	dimsf[1] = NY;
-	H5::DataSpace dataspace(RANK, dimsf);
-	H5::IntType datatype(H5::PredType::NATIVE_INT);
-	datatype.setOrder(H5T_ORDER_LE);
-
-	H5::DataSet dataset = file.createDataSet(DATASET_NAME, datatype, dataspace);
-	dataset.write(data, H5::PredType::NATIVE_INT);
-
-	std::string file_name = file.getFileName();
-	EXPECT_TRUE(file_name == FILE_NAME);
 
 }
