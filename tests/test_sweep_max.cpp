f71b7cd6b21fff6bca318f538375921839ffbb09
Thomas Krijnen <t.krijnen@gmail.com>
2022-04-04 14:19:33 +0200

    Add missing test

---
+++ tests/test_sweep_max.cpp
@@ -0,0 +1,23 @@
+#include "../storage.h"
+#include "../sweep.h"
+#include <gtest/gtest.h>
+
+TEST(Sweep, SweepWithMax) {
+	const double d = 1;
+	auto a = new chunked_voxel_storage<bit_t>(0, 0, 0, d, 20, 20, 20, 10);
+	a->Set(make_vec(1U, 1U, 1U));
+	auto b = new chunked_voxel_storage<bit_t>(0, 0, 0, d, 20, 20, 20, 10);
+	b->Set(make_vec(1U, 1U, 10U));
+
+	{
+		sweep s;
+		s.until = b;
+		ASSERT_EQ(s(a, 0, 0, 1)->count(), 10 - 1);
+	}
+	{
+		sweep s;
+		s.until = b;
+		s.max_depth = 3;
+		ASSERT_EQ(s(a, 0, 0, 1)->count(), 1 + 3);
+	}
+}
